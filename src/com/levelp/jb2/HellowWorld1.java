4c15b591a2d433a35e31fda5f04131cf69e661af
wlad00 <wladsliw@list.ru>
2017-07-27 13:06:29 +0300

    two java classes

---
+++ src/com/levelp/jb2/HellowWorld1.java
@@ -0,0 +1,11 @@
+package com.levelp.jb2;
+
+/**
+ * Created by Vsevolod on 27.07.17.
+ */
+public class HellowWorld1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow, world!");
+    }
+}
